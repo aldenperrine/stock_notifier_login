a3074cef0a9f28f41e5a211404b383d0b1555cc4
Alden Perrine <aldenperrine@gmail.com>
2019-06-05 11:16:49 -0700

    Added AES encryption

--- java/com/stock_client_login/ClientCrypto.java
+++ java/com/stock_client_login/ClientCrypto.java
@@ -2,21 +2,35 @@ package com.stock_client_login;
 
 import java.util.Base64;
 import javax.crypto.Cipher;
+import javax.crypto.KeyGenerator;
+import javax.crypto.SecretKey;
 import javax.crypto.spec.SecretKeySpec;
+import javax.crypto.spec.IvParameterSpec;
 import java.security.*;
+import java.security.SecureRandom;
 import java.security.spec.X509EncodedKeySpec;
 import java.lang.System.*;
 
 public class ClientCrypto {
-    private final String publicKeyString = "MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAnQGlH1nBCp8Fj31GpMjeQh4YQ5fz2SPwJQSMAUpRLrPMCRvb2jvxKeRO4nw4anIzXR0Xaao1UV0obIaJT87sayE6HfHLH3pXTT7psJj0QWS6R/kKVWT5X/i5RT3C7sWJnIs/XlKCADc2t9YzWcbPjDPyR6tXTURXzJ/RZbValGBK3CSVYopOTI+Y/8fVC1XoW1NQynf7ShDwoHvhpBPh916ezCz11le6kz0HW2X2qTHT2Xg+xEkgCzt4xsVdT5M7TpI6foedR8HRiVE7M212wjD7j5wis4bQgsLWGMoFWWEOWpsFl9HESicqnyZObz+uymw4ND8VuxgDaz6wXmEzkwIDAQAB";
+    private static final String publicKeyString = "MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAnQGlH1nBCp8Fj31GpMjeQh4YQ5fz2SPwJQSMAUpRLrPMCRvb2jvxKeRO4nw4anIzXR0Xaao1UV0obIaJT87sayE6HfHLH3pXTT7psJj0QWS6R/kKVWT5X/i5RT3C7sWJnIs/XlKCADc2t9YzWcbPjDPyR6tXTURXzJ/RZbValGBK3CSVYopOTI+Y/8fVC1XoW1NQynf7ShDwoHvhpBPh916ezCz11le6kz0HW2X2qTHT2Xg+xEkgCzt4xsVdT5M7TpI6foedR8HRiVE7M212wjD7j5wis4bQgsLWGMoFWWEOWpsFl9HESicqnyZObz+uymw4ND8VuxgDaz6wXmEzkwIDAQAB";
+    private static final int AES_BLOCK_SIZE = 32;
+    private static final int AES_RAND_SIZE = 16;
     
     private Cipher encrypter;
     private Base64.Encoder encoder;
+    private Base64.Decoder decoder;
+    private byte[] iv;
+    private IvParameterSpec ivSpec;
+    private SecretKeySpec skey;
     
     public static void main(String[] args) {
 	try {
 	    System.out.println((new ClientCrypto()).encrypt(args[0]));
-	    
+	    System.out.println((new ClientCrypto()).encrypt(args[0]));
+	    ClientCrypto crypto = new ClientCrypto();
+	    crypto.setAesKey(Base64.getDecoder().decode("p5R0Bwkokg1hlvN709mT67EV3U1w4g7f0hEBMJnuSzE=".getBytes("ascii")));
+	    System.out.println(crypto.aes_encrypt("cats"));
+	    System.out.println(crypto.aes_decrypt("lsrOG+qlzWeRjzNxgKiQTrRfLQC44mYYI+La07nI23Y="));
 	} catch (Exception e) {
 	    e.printStackTrace();
 	    System.out.println(e);
@@ -24,19 +38,63 @@ public class ClientCrypto {
     }
 
     public ClientCrypto() throws Exception {
+	encoder = Base64.getEncoder();
+	decoder = Base64.getDecoder();
+
 	PublicKey pub = makePublicKey();
 	encrypter = Cipher.getInstance("RSA/ECB/OAEPWithSHA-256AndMGF1Padding");
 	encrypter.init(Cipher.ENCRYPT_MODE, pub);
 
-	encoder = Base64.getEncoder();
+	iv = new byte[AES_RAND_SIZE];
+	(new SecureRandom()).nextBytes(iv);
+	ivSpec = new IvParameterSpec(iv);
     }
 
-    public String encrypt(String plaintext) throws GeneralSecurityException {
-	return new String(encoder.encode(encrypter.doFinal(plaintext.getBytes())));
+    public String encrypt(String plaintext) throws Exception {
+	return new String(encoder.encode(encrypter.doFinal(plaintext.getBytes("UTF-8"))));
     }
     
+    public void setAesKey(byte[] key) throws Exception {
+	if (key.length != 32)
+	    return;
+	skey = new SecretKeySpec(key, "AES");
+    }
+
+    public String aes_encrypt(String plaintext) throws Exception {
+	Cipher ci = Cipher.getInstance("AES/CBC/PKCS5Padding");
+	ci.init(Cipher.ENCRYPT_MODE, skey, ivSpec);
+	byte[] ct = ci.doFinal(plaintext.getBytes("UTF-8"));
+	byte[] ctiv = new byte[ct.length+iv.length];
+	for (int i = 0; i < iv.length; ++ i)
+	    ctiv[i] = iv[i];
+	for (int i = 0; i < ct.length; ++ i)
+	    ctiv[i+iv.length] = ct[i];
+	return new String(encoder.encode(ctiv));
+    }
+    
+    public String aes_decrypt(String ciphertext) throws Exception {	
+	byte[] ctiv = decoder.decode(ciphertext);
+	
+	byte[] siv = new byte[AES_RAND_SIZE];
+	for (int i = 0; i < siv.length; ++i)
+	    siv[i] = ctiv[i];
+
+	byte[] ct = new byte[ctiv.length - AES_RAND_SIZE];
+	for (int i = 0; i < ct.length; ++i)
+	    ct[i] = ctiv[i+AES_RAND_SIZE];
+	
+	IvParameterSpec sivSpec = new IvParameterSpec(siv);
+	
+	Cipher ci = Cipher.getInstance("AES/CBC/PKCS5Padding");
+	ci.init(Cipher.DECRYPT_MODE, skey, sivSpec);
+	byte[] pt = ci.doFinal(ct);
+
+	return new String(pt, "UTF-8");
+    }
+
+
     public PublicKey makePublicKey() throws Exception {
-	byte[] keyBytes = Base64.getDecoder().decode(publicKeyString);
+	byte[] keyBytes = decoder.decode(publicKeyString.getBytes("ascii"));
 
 	X509EncodedKeySpec spec =
 	    new X509EncodedKeySpec(keyBytes);
--- py/crypto.py
+++ py/crypto.py
@@ -1,9 +1,13 @@
 import sys
 from Crypto.PublicKey import RSA
-from Crypto.Cipher import PKCS1_OAEP
+from Crypto.Cipher import PKCS1_OAEP, AES
 from Crypto.Hash import SHA256, SHA1
 from Crypto.Signature import PKCS1_v1_5, pss
+from Crypto import Random
 from base64 import b64encode, b64decode
+
+AES_BLOCK_SIZE = 32
+AES_RAND_SIZE = 16
 
 class CryptoLib:
     def __init__(self, keypath):
@@ -19,8 +23,25 @@ class CryptoLib:
         digest = SHA256.new()
         digest.update(message)
         return self.signer.sign(digest)
+
+    def aes_encrypt(self, key, plaintext):
+        plaintext = self._aes_pad(plaintext)
+        rand = Random.new().read(AES_RAND_SIZE)
+        encrypter = AES.new(b64decode(key), AES.MODE_CBC, rand)
+        return b64encode(rand+encrypter.encrypt(plaintext))
+
+    def aes_decrypt(self, key, ciphertext):
+        ciphertext = b64decode(ciphertext)
+        rand = ciphertext[:AES_RAND_SIZE]
+        decrypter = AES.new(b64decode(key), AES.MODE_CBC, rand)
+        return self._aes_unpad(decrypter.decrypt(ciphertext[AES_RAND_SIZE:])).decode('utf-8')
         
+    def _aes_pad(self, s):
+        return bytes(s + (AES_BLOCK_SIZE - len(s) % AES_BLOCK_SIZE) * chr(AES_BLOCK_SIZE - len(s) % AES_BLOCK_SIZE), 'utf-8')
         
+    def _aes_unpad(self, c):
+        return c[:-ord(c[-1:])]
+    
 if __name__ == "__main__":
     if len(sys.argv) < 3:
         print("Not enough args")
